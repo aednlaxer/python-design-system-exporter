47ee870b0623de536f684147446789feb0243452
alex <alexander.troshkov@qvik.fi>
2018-11-22 22:06:11 +0200

    Add exporter.py

---
+++ exporter.py
@@ -0,0 +1,93 @@
+import json
+import sys
+
+from python_sketch_api.sketch_api import SketchFile
+from python_sketch_api.sketch_types import SJTextLayer, SJSymbolMaster, TextAlignmentEnum
+
+
+def colour(color_component):
+    return {
+        "red": round(color_component.red * 255),
+        "green": round(color_component.green * 255),
+        "blue": round(color_component.blue * 255)
+    }
+
+
+def gravity(alignment):
+    if alignment == TextAlignmentEnum.Left:
+        return "left"
+    elif alignment == TextAlignmentEnum.Right:
+        return "right"
+    elif alignment == TextAlignmentEnum.Center:
+        return "center"
+    elif alignment == TextAlignmentEnum.Justified:
+        return "justified"
+    else:
+        return None
+
+
+def shadows(shadowsList):
+    if shadowsList is None:
+        return None
+
+    s = []
+    for shadow in shadowsList:
+        if shadow.isEnabled:
+            s.append({
+                "blur-radius": shadow.blurRadius,
+                "color": shadow.color,
+                "offset-x": shadow.offsetX,
+                "offset-y": shadow.offsetY
+            })
+
+    return s
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: ./exporter.py file.sketch")
+        return
+
+    filename = sys.argv[1]
+    file = SketchFile.from_file(filename)
+
+    for page in file.sketch_pages:
+
+        # Typography page contains all text styles
+        if page.name == "Typography":
+            styles = []
+
+            for layer in page.layers:
+                style = {}
+
+                if type(layer) is SJSymbolMaster:
+                    style['style-name'] = layer.name
+
+                    for subLayer in layer.layers:
+                        if type(subLayer) is SJTextLayer:
+                            style['background'] = subLayer.backgroundColor
+
+                            if subLayer.style.borders is not None:
+                                for border in subLayer.style.borders:
+                                    style['border-color'] = colour(border.color)
+                                    style['border-thickness'] = border.thickness
+
+                            style['shadows'] = shadows(subLayer.style.shadows)
+
+                            for attr in subLayer.attributedString.attributes:
+                                font = attr.attributes.MSAttributedStringFontAttribute.attributes
+                                color = attr.attributes.MSAttributedStringColorAttribute
+                                style['font-family'] = font.name
+                                style['font-size'] = font.size
+                                style['gravity'] = gravity(attr.attributes.paragraphStyle.alignment)
+                                style['text-color'] = colour(color)
+
+                        styles.append(style)
+
+            print(json.dumps(styles, sort_keys=True, indent=4))
+
+            break
+
+
+if __name__ == "__main__":
+    main()
